d002852e1bcb90d3ae6ddaf944afc5db480ac072
Ben Found <ben@well-made.uk>
2025-04-21 19:51:13 +0100

    Initial

---
+++ tailwind.preset.js
@@ -0,0 +1,15 @@
+// tailwind.preset.js
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        highlight: '#646cff',
+        'highlight-dark': '#3a3c6b',
+      },
+      borderRadius: {
+        ecosystem: '1.25rem', // Example custom radius
+      },
+    },
+  },
+  plugins: [],
+};
